0549cb4a2899e30b2cfb3357f2fe2d62dc176e51
DanielMutton <daniel.mutton@hotmail.com>
2017-02-02 11:36:53 -0500

    Raster to numpy array

---
+++ raster to arcpy array.py
@@ -0,0 +1,75 @@
+PythonWin 2.7.10 (default, May 23 2015, 09:44:00) [MSC v.1500 64 bit (AMD64)] on win32.
+Portions Copyright 1994-2008 Mark Hammond - see 'Help/About PythonWin' for further copyright information.
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 6, in <module>
+    WorkSpace = input()
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\app.py", line 375, in Win32Input
+    return eval(raw_input(prompt))
+  File "<string>", line 1
+    S:\100934905\GEOM 4009\002C02_cdsm_final
+     ^
+SyntaxError: invalid syntax
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 8, in <module>
+    FileObject = open(filename,'r')
+NameError: name 'filename' is not defined
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 8, in <module>
+    FileObject = open(FileName,'r')
+IOError: [Errno 2] No such file or directory: '002.tif'
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 6, in <module>
+    WorkSpace = raw_input()
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\app.py", line 370, in Win32RawInput
+    raise KeyboardInterrupt("operation cancelled")
+KeyboardInterrupt: operation cancelled
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 8, in <module>
+    os.chdir(path)
+NameError: name 'os' is not defined
+>>> S:\100934905\GEOM 4009\002C02_cdsm_final
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 15, in <module>
+    Raster = arcpy.Raster(FileObject)
+TypeError: expected a raster or layer name
+>>> S:\100934905\GEOM 4009\002C02_cdsm_final
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 15, in <module>
+    Raster = arcpy.Raster(FileObject)
+TypeError: expected a raster or layer name
+>>> S:\100934905\GEOM 4009\002C02_cdsm_final
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 15, in <module>
+    Raster = arcpy.Raster(FileObject)
+TypeError: expected a raster or layer name
+>>> S:\100934905\GEOM 4009\002C02_cdsm_final
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 15, in <module>
+    Raster = arcpy.Raster(FileObject)
+TypeError: expected a raster or layer name
+>>> S:\100934905\test
+Traceback (most recent call last):
+  File "C:\Python27\ArcGISx6410.4\Lib\site-packages\pythonwin\pywin\framework\scriptutils.py", line 326, in RunScript
+    exec codeObject in __main__.__dict__
+  File "D:\Student\100934905\Raster to numpy.py", line 15, in <module>
+    Raster = arcpy.Raster(FileObject)
+TypeError: expected a raster or layer name
+>>> 
